c21d4330c13425f7bfeadb68263ef835efd09dd4
shakilkhan2 <dev.shakilkhan.com>
2023-10-20 18:34:40 +0600

    tailwind should be fixed

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,12 +3,13 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- @vite('resources/css/app.css') -->
     <link rel="shortcut icon" href="./assets/icons/star.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <title>MediaStar</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
         /* Define a custom CSS class for the input fields and textarea */
         .input-field {
